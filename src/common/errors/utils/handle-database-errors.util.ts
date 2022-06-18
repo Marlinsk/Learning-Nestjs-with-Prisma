3571d2c2269a09ac03908d6d7c544d48242f1e72
Marlon <Marlonnlmx@outlook.com>
2022-06-18 00:28:02 -0300

    Implementing utils

---
+++ src/common/errors/utils/handle-database-errors.util.ts
@@ -0,0 +1,17 @@
+import { DataBaseError } from '../@types/DataBaseError';
+import { PrismaClientError } from '../@types/PrismaClientError';
+import { UniqueConstraintError } from '../@types/UniqueConstraintError';
+
+enum PrismaErrors {
+  UniqueConstraintFail = 'P2002',
+}
+
+export const handleDatabaseErrors = (e: PrismaClientError): Error => {
+  switch (e.code) {
+    case PrismaErrors.UniqueConstraintFail:
+      return new UniqueConstraintError(e);
+
+    default:
+      return new DataBaseError(e.message);
+  }
+};
