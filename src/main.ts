e727a5b086a476f971515e2c3cbe1f79163668da
Marlon <Marlonnlmx@outlook.com>
2022-06-25 01:14:34 -0300

    Implementing swagger

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,20 @@ import { DataBaseInterceptor } from './common/errors/interceptors/database.inter
 import { NotFoundErrorInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { UnauthorizedErrorInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Blog')
+    .setDescription('The Simple Blog API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
